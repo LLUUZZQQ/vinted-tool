6dd58493fe4539aead7f14f64311a29f43aba276
LLUUZZQQ <lzq13328903829@163.com>
2026-05-18 02:48:07 +0800

    v1.0.1: QDialog import fix

--- update_checker.py
+++ update_checker.py
@@ -16,7 +16,7 @@ import subprocess
 UPDATE_URL = "https://raw.githubusercontent.com/LLUUZZQQ/vinted-tool/main/update.json"
 
 # 当前版本
-CURRENT_VERSION = "1.0.0"
+CURRENT_VERSION = "1.0.1"
 
 
 def _fetch_json(url, timeout=10):
