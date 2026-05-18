c3b53520dbddb6e8d397f42625fc2615960c4210
LLUUZZQQ <lzq13328903829@163.com>
2026-05-19 04:48:58 +0800

    v2.5.3: Fix title bar layout stretch

--- update_checker.py
+++ update_checker.py
@@ -16,7 +16,7 @@ import subprocess
 UPDATE_URL = "https://vt-proxy.vtmax.workers.dev/update.json"
 
 # 当前版本
-CURRENT_VERSION = "2.5.2"
+CURRENT_VERSION = "2.5.3"
 
 
 def _fetch_json(url, timeout=10):
